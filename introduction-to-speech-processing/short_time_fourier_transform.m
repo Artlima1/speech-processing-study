5a370bf155d5f5c999161c991668bf3aece6998f
ArthurLima <arthurdeolima@gmail.com>
2024-07-26 01:21:00 -0300

    Vowel synthesis and better charts

--- introduction-to-speech-processing/short_time_fourier_transform.m
+++ introduction-to-speech-processing/short_time_fourier_transform.m
@@ -8,23 +8,29 @@ figure
 subplot(3, 2, 1)
 speccomp(s5, 3750, [401, 201, 101, 51], 512, 30)
 title('Short-Time Spectra in Different Window Lengths: ncenter=3750, original signal');
+legend("401", "201", "101", "51")
 subplot(3, 2, 2)
 speccomp(s5_filt, 3750, [401, 201, 101, 51], 512, 30)
 title('Short-Time Spectra in Different Window Lengths: ncenter=3750, filtered signal');
+legend("401", "201", "101", "51")
 
 subplot(3, 2, 3)
 speccomp(s5, 16100, [401, 201, 101, 51], 512, 30)
 title('Short-Time Spectra in Different Window Lengths: ncenter=16100, original signal');
+legend("401", "201", "101", "51")
 subplot(3, 2, 4)
 speccomp(s5_filt, 16100, [401, 201, 101, 51], 512, 30)
 title('Short-Time Spectra in Different Window Lengths: ncenter=16100, filtered signal');
+legend("401", "201", "101", "51")
 
 subplot(3, 2, 5)
 speccomp(s5, 17200, [401, 201, 101, 51], 512, 30)
 title('Short-Time Spectra in Different Window Lengths: ncenter=17200, original signal');
+legend("401", "201", "101", "51")
 subplot(3, 2, 6)
 speccomp(s5_filt, 17200, [401, 201, 101, 51], 512, 30)
 title('Short-Time Spectra in Different Window Lengths: ncenter=17200, filtered signal');
+legend("401", "201", "101", "51")
 
 % É possível perceber claramente o aumento da resolução em frequência com o
 % aumento do tamanho da janela, embora isso diminua a resolução temporal.
@@ -35,23 +41,29 @@ figure
 subplot(3, 2, 1)
 stspect(s5,3750,200,401,512,10,20)
 title('Short-Time Spectra in Different n values: ncenter=3750, original signal');
+legend()
 subplot(3, 2, 2)
 stspect(s5_filt,3750,200,401,512,10,20)
 title('Short-Time Spectra in Different n values: ncenter=3750, filtered signal');
+legend()
 
 subplot(3, 2, 3)
 stspect(s5,16100,200,401,512,10,20)
 title('Short-Time Spectra in Different n values: ncenter=16100, original signal');
+legend()
 subplot(3, 2, 4)
 stspect(s5_filt,16100,200,401,512,10,20)
 title('Short-Time Spectra in Different n values: ncenter=16100, filtered signal');
+legend()
 
 subplot(3, 2, 5)
 stspect(s5,17200,200,401,512,10,20)
 title('Short-Time Spectra in Different n values: ncenter=17200, original signal');
+legend()
 subplot(3, 2, 6)
 stspect(s5_filt,17200,200,401,512,10,20)
 title('Short-Time Spectra in Different n values: ncenter=17200, filtered signal');
+legend()
 
 % É possível perceber nos gráficos, especialmente com ncenter=16100, a forma como o 
 % filtro evidencia altas frequências, principalmente pelo efeito de amortecimento
--- speech-modeling/glottal_pulse_models.m
+++ speech-modeling/glottal_pulse_models.m
@@ -1,6 +1,4 @@
 % % % % % % % % % % % % %  1) Comparison of Glottal Pulse Models % % % % % % % % %
-% TODO - Determine analytical relationship between Gflip and G. Show that
-% if signal is maximum-phase, flipping it produces minimum phase
 
 a = 0.91;
 N1 = 45;
@@ -16,14 +14,17 @@ gR_flip = fliplr(gR);
 GR_flip = fft(gR_flip, Nfreq);
 
 figure
+subplot(1, 2, 1)
 plot(n, gR)
 hold on
 plot(n, gE)
 plot(n, gR_flip)
-legend('GR', 'GE', 'GR flip');
+title("Pulsos Glotais")
+legend('g_R', 'g_E', 'g_R flip');
+xlabel('n')
 hold off
 
-figure
+subplot(1, 2, 2)
 plot(WR, 20*log10(abs(GR)))
 hold on
 plot(WE, 20*log10(abs(GE)))
@@ -31,15 +32,30 @@ plot(WR, 20*log10(abs(GR_flip)))
 legend('GR', 'GE', 'GR_flip');
 xlabel('Frequency (rad/s)')
 ylabel('Log Magnitude in dB')
+title("Resposta em frequência dos modelos de pulso")
 hold off
 
 rPulsesCases([10, 15, 25], 50, Nfreq);
 
-roots_GR = roots(GR);
-roots_GR_flip = roots(GR_flip);
+coefficients_GR = lpc(gR, 15);
+roots1_GR = roots(coefficients_GR);
+coefficients_GR_flip = lpc(gR_flip, 15);
+roots1_GR_flip = roots(coefficients_GR_flip);
+
+roots2_GR = roots(GR);
+roots2_GR_flip = roots(GR_flip);
 
 figure
-subplot(2,1,1)
-zplane([],roots_GR)
-subplot(2,1,2)
-zplane([],roots_GR_flip)
+subplot(2,2,1)
+zplane([],roots1_GR)
+title("Raizes do pulso GR (usando LPC)")
+subplot(2,2,2)
+zplane([],roots1_GR_flip)
+title("Raizes do pulso GR Flip (usando LPC)")
+
+subplot(2,2,3)
+zplane([],roots2_GR)
+title("Raizes do pulso GR (uso direto roots())")
+subplot(2,2,4)
+zplane([],roots2_GR_flip)
+title("Raizes do pulso GR Flip (uso direto roots())")
